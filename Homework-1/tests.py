d54d01f179385d0536ce3c40d86356c9e462b4bf
Ryan <pakr@oregonstate.edu>
2024-04-22 13:43:12 -0700

    Added Visa card test

--- Homework-1/tests.py
+++ Homework-1/tests.py
@@ -3,10 +3,28 @@ from credit_card_validator import credit_card_validator
 
 class TestCreditCardValidator(unittest.TestCase):
 
-    def test11(self):
+    def testexample(self):
         """Verifies if Master Cards with valid lengths and invalid check bits returns False
         Picked using Category Partition Testing"""
         self.assertFalse(credit_card_validator("...."))
+
+    def test1(self):
+        """Verifies if Visa cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("4012340123401234"))
+
+    def test2(self):
+        """Verifies if Visa cards with valid lengths and invalid prefixes returns False."""
+        self.assertFalse(credit_card_validator("7012340123401234"))
+
+    def test1(self):
+        """Verifies if Visa cards with invalid lengths and valid prefixes returns False."""
+        self.assertFalse(credit_card_validator("40123401234"))
+
+    def test1(self):
+        """Verifies if Visa cards with invalid lengths and invalid prefixes returns False."""
+        self.assertFalse(credit_card_validator("701234012340"))
+
+    
 
 if __name__ == "__main__":
     unittest.main()

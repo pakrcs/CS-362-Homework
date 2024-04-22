cad4213f84620c03d8bc9f17575d0c1c1d43783c
Ryan <pakr@oregonstate.edu>
2024-04-22 14:35:06 -0700

    Added valid Master Card prefix and lengths

--- Homework-1/tests.py
+++ Homework-1/tests.py
@@ -3,11 +3,6 @@ from credit_card_validator import credit_card_validator
 
 class TestCreditCardValidator(unittest.TestCase):
 
-    def testexample(self):
-        """Verifies if Master Cards with valid lengths and invalid check bits returns False
-        Picked using Category Partition Testing"""
-        self.assertFalse(credit_card_validator("...."))
-
     def test1(self):
         """Verifies if Visa cards with valid lengths and valid prefixes returns True."""
         self.assertTrue(credit_card_validator("4012340123401234"))
@@ -16,15 +11,49 @@ class TestCreditCardValidator(unittest.TestCase):
         """Verifies if Visa cards with valid lengths and invalid prefixes returns False."""
         self.assertFalse(credit_card_validator("7012340123401234"))
 
-    def test1(self):
+    def test3(self):
         """Verifies if Visa cards with invalid lengths and valid prefixes returns False."""
         self.assertFalse(credit_card_validator("40123401234"))
 
-    def test1(self):
+    def test4(self):
         """Verifies if Visa cards with invalid lengths and invalid prefixes returns False."""
         self.assertFalse(credit_card_validator("701234012340"))
 
-    
+    def test5(self):
+        """Verifies if MasterCard cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("5112340123401234"))
+
+    def test6(self):
+        """Verifies if MasterCard cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("5212340123401234"))
+
+    def test7(self):
+        """Verifies if MasterCard cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("5312340123401234"))
+
+    def test8(self):
+        """Verifies if MasterCard cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("5412340123401234"))
+
+    def test9(self):
+        """Verifies if MasterCard cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("5512340123401234"))
+
+    def test10(self):
+        """Verifies if MasterCard cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("2221123412341234"))
+
+    def test11(self):
+        """Verifies if MasterCard cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("2221123412341234"))
+
+    def test12(self):
+        """Verifies if MasterCard cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("2720123412341234"))
+
+    def test13(self):
+        """Verifies if MasterCard cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("2500123412341234"))
 
 if __name__ == "__main__":
     unittest.main()

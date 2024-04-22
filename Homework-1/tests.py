29e6a24d9cc6d5891f7d8967f607ebc344fe8db8
Ryan <pakr@oregonstate.edu>
2024-04-22 14:42:03 -0700

    Added AMEX tests

--- Homework-1/tests.py
+++ Homework-1/tests.py
@@ -55,5 +55,37 @@ class TestCreditCardValidator(unittest.TestCase):
         """Verifies if MasterCard cards with valid lengths and valid prefixes returns True."""
         self.assertTrue(credit_card_validator("2500123412341234"))
 
+    def test14(self):
+        """Verifies if MasterCard cards with valid lengths and invalid prefixes returns False."""
+        self.assertFalse(credit_card_validator("5912340123401234"))
+
+    def test14(self):
+        """Verifies if MasterCard cards with invalid lengths and valid prefixes returns False."""
+        self.assertFalse(credit_card_validator("51123401234"))
+
+    def test14(self):
+        """Verifies if MasterCard cards with invalid lengths and invalid prefixes returns False."""
+        self.assertFalse(credit_card_validator("59123401234"))
+
+    def test15(self):
+        """Verifies if American Express cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("341234012340123"))
+
+    def test16(self):
+        """Verifies if American Express cards with valid lengths and valid prefixes returns True."""
+        self.assertTrue(credit_card_validator("371234012340123"))
+
+    def test17(self):
+        """Verifies if American Express cards with valid lengths and invalid prefixes returns False."""
+        self.assertFalse(credit_card_validator("321234012340123"))
+
+    def test18(self):
+        """Verifies if American Express cards with invalid lengths and valid prefixes returns False."""
+        self.assertFalse(credit_card_validator("341234012340123401234"))
+
+    def test19(self):
+        """Verifies if American Express cards with invalid lengths and invalid prefixes returns False."""
+        self.assertFalse(credit_card_validator("39123401234"))
+
 if __name__ == "__main__":
     unittest.main()

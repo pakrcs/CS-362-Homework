5fe58631d701ae921eab4b8bd1b6fdb366869edf
Ryan <pakr@oregonstate.edu>
2024-04-22 18:30:47 -0700

    Added Visa boundary and edges tests

--- Homework-1/tests.py
+++ Homework-1/tests.py
@@ -3,13 +3,17 @@ from credit_card_validator import credit_card_validator
 
 class TestCreditCardValidator(unittest.TestCase):
 
+    def test0(self):
+        """Verifies if an empty credit card returns False."""
+        self.assertFalse(credit_card_validator(""))
+
     def test1(self):
         """Verifies if Visa cards with valid lengths and valid prefixes returns True."""
         self.assertTrue(credit_card_validator("4012340123401234"))
 
     def test2(self):
         """Verifies if Visa cards with valid lengths and invalid prefixes returns False."""
-        self.assertFalse(credit_card_validator("7012340123401234"))
+        self.assertFalse(credit_card_validator("5012340123401234"))
 
     def test3(self):
         """Verifies if Visa cards with invalid lengths and valid prefixes returns False."""
@@ -86,6 +90,42 @@ class TestCreditCardValidator(unittest.TestCase):
     def test19(self):
         """Verifies if American Express cards with invalid lengths and invalid prefixes returns False."""
         self.assertFalse(credit_card_validator("39123401234"))
+
+    def test20(self):
+        """Verifies if Visa cards have non-numeric characters returns False."""
+        self.assertFalse(credit_card_validator("4042318abc171915"))
+
+    def test21(self):
+        """Verifies if Visa cards have non-numeric characters returns False."""
+        self.assertFalse(credit_card_validator("4042-3180-3147-1915"))
+
+    def test22(self):
+        """Verifies if Visa cards have white space in between numbers returns False."""
+        self.assertFalse(credit_card_validator("4042 3180 3147 1915"))
+
+    def test23(self):
+        """Verifies if Visa cards have white space before numbers returns False."""
+        self.assertFalse(credit_card_validator("    4042318031471915"))
+
+    def test24(self):
+        """Verifies if Visa cards have white space after numbers returns False."""
+        self.assertFalse(credit_card_validator("4042318031471915     "))
+
+    def test25(self):
+        """Verifies if Visa cards with valid lengths and invalid prefixes returns False."""
+        self.assertFalse(credit_card_validator("3012340123401234"))
+
+    def test26(self):
+        """Verifies if Visa cards with invalid lengths and valid prefixes returns False."""
+        self.assertFalse(credit_card_validator("40123401234642869"))
+
+    def test27(self):
+        """Verifies Visa card edge cases if valid returns True."""
+        self.assertTrue(credit_card_validator("4000123412341234"))
+        
+    def test28(self):
+        """Verifies Visa card edge cases if valid returns True."""
+        self.assertTrue(credit_card_validator("4999123412341234"))
 
 if __name__ == "__main__":
     unittest.main()

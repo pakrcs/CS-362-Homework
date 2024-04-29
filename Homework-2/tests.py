8bbaddb5d40b9bd67582ba9d44ef7d9e4c104948
Ryan <pakr@oregonstate.edu>
2024-04-29 12:38:27 -0700

    added 8 tests, changed to proper attribute TestCase

--- Homework-2/tests.py
+++ Homework-2/tests.py
@@ -2,7 +2,7 @@ import unittest
 from contrived_func import contrived_func
 
 
-class TestContrivedFunc(unittest.testcase):
+class TestContrivedFunc(unittest.TestCase):
 
     def test1(self):
 
@@ -18,7 +18,23 @@ class TestContrivedFunc(unittest.testcase):
 
     def test4(self):
 
+        contrived_func(1)
+
+    def test5(self):
+
         contrived_func(11)
+
+    def test6(self):
+
+        contrived_func(6)
+
+    def test7(self):
+
+        contrived_func(7)
+
+    def test8(self):
+
+        contrived_func(10)
 
 
 if __name__ == '__main__':

6e6bbd48597fbd07f517ef23efb37efa2d2c400d
Ryan <pakr@oregonstate.edu>
2024-05-05 20:31:20 -0700

    update

--- Homework-3/tests.py
+++ Homework-3/tests.py
@@ -5,22 +5,26 @@ from credit_card_validator import credit_card_validator
 
 
 class TestCase(unittest.TestCase):
+
     pass
 
 
 def build_test_func(test_case, func_under_test):
+
     def test(self):
+
         result = func_under_test(test_case)
-        self.assertIsInstance(result, bool, f"The credit card number is valid: {result}")
+        self.assertIsInstance(result, bool, 'The credit card number is valid: '+ str(result))
+
     return test
 
 
-def generate_testcases(tests_to_generate=600000):
+def generate_testcases(tests_to_generate=450000):
     for i in range(tests_to_generate):
         length = random.randint(14, 17)
         card_number = ''.join(random.choices(string.digits, k=length))
-        
-        test_func_name = f'test_{i}'
+
+        test_func_name = 'test_' + str(i)
         test_case_func = build_test_func(card_number, credit_card_validator)
         setattr(TestCase, test_func_name, test_case_func)
 

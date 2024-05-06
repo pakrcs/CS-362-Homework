da3dfc5551fff48761f61dd852b56b50833b1c9d
Ryan <pakr@oregonstate.edu>
2024-05-05 19:31:06 -0700

    got rid of prefixes and lengths and instead generate random numbers

--- Homework-3/tests.py
+++ Homework-3/tests.py
@@ -8,46 +8,21 @@ class TestCase(unittest.TestCase):
     pass
 
 
-def build_test_func(expected, test_case, func_under_test, message):
+def build_test_func(test_case, func_under_test):
     def test(self):
         result = func_under_test(test_case)
-        self.assertEqual(expected, result, message.format(test_case, expected, result))
+        self.assertIsInstance(result, bool, f"Value returned is not a bool: {result}")
     return test
 
 
-def generate_testcases(tests_to_generate=100):
+def generate_testcases(tests_to_generate=1000):
     for i in range(tests_to_generate):
-        expected = True
-        # List of edge case lengths
-        edge_cases = [7,8,9,19,20,21]
-        # 50% chance of generating an edge case
-        odds = random.randint(0,1)
-        if odds == 1:
-            length = random.choice(edge_cases)
-        else:
-            # Random length
-            length = random.randint(0,30)
-        # Random number of lower case
-        low = random.randint(0, length)
-        # Random number of upper case
-        up = random.randint(0, length - low)
-        # Random number of numbers
-        dig = random.randint(0, length - low - up)
-        # Random number of symbols
-        sym = random.randint(0, length - low - up - dig)
-        # Determine final length of string
-        length = low + up + dig + sym
-        # Set expected result based on specification
-        if length < 8 or length > 20:
-            expected = False
-        if low < 1 or up < 1 or dig < 1 or sym < 1:
-            expected = False
-        # Generate password
-        pwd = gen_pass(length, low, up, dig, sym)
-        # Build test function
-        message = 'Test case: {}, Expected: {}, Result: {}'
-        new_test = build_test_func(expected, pwd, check_pwd, message)
-        setattr(TestCase, 'test_{}'.format(pwd), new_test)
+        length = random.randint(14, 17)
+        card_number = ''.join(random.choices(string.digits, k=length))
+        
+        test_func_name = f'test_{i}'
+        test_case_func = build_test_func(card_number, credit_card_validator)
+        setattr(TestCase, test_func_name, test_case_func)
 
 
 if __name__ == '__main__':
